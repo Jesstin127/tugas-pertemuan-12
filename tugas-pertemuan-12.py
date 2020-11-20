95321191acfdfee3c6ee639fc4dc4917b4379b58
Jesstin127 <jjodhanto@gmail.com>
2020-11-20 21:13:16 +0700

    add tugas-pertemuan-12

---
+++ tugas-pertemuan-12.py
@@ -0,0 +1,56 @@
+kata = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',]
+
+def num(n:int):
+    if n < 10 :
+        return kata[n]
+    elif n >= 1_000_000_000:
+        return num(n//1_000_000_000)+ ' billion '+ num(n%1_000_000_000)
+    elif n >= 1_000_000:
+        return num(n//1_000_000)+' million ' + num(n%1_000_000)
+    elif n>=1_000:
+        return num(n//1000) + ' thousand ' + num(n%1000)
+    elif n>=100:
+        return num(n//100)+ ' hundred ' + num(n%100)
+    elif n>=20:
+        if n//10 == 2:
+            return 'twenty ' + num(n%10)
+        elif n//10 == 3:
+            return 'thirty ' + num(n%10)
+        elif n//10 == 5:
+            return 'fifty ' + num(n%10)
+        else :
+            return num(n//10)+('ty 'if (n//10)!=8 else 'y ') + num(n%10)
+    else :
+        if n==10:
+            return 'ten'
+        elif n==11:
+            return 'eleven'
+        elif n==12:
+            return 'twelve'
+        elif n==13:
+            return 'thirteen'
+        elif n==15:
+            return 'fifteen'
+        else :
+            return num(n%10) + 'teen'
+
+
+import os
+while True:
+    os.system('cls')
+    print("NUMBERS CONVERTER INTO WORDS")
+    print("============================")
+    try:
+        bil=int(input("Number\t? "))
+        print(f"{bil:,} = {num(bil)}")
+    except:
+        print("INPUT MUST BE NUMBER !!!")
+    print()
+    print("Want to Convert Again [y/n] ? ",end='')
+    again=input()
+    print()
+    if again.lower() == 'n' :
+        break
+
+
+
